ee9341fe04efa6fd18bb3bc26911486b9757415a
leolee2708 <leho.vietquan@gmail.com>
2021-02-09 15:14:47 -0500

    fixed homepage clicked

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -4,9 +4,9 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 let isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("members");
@@ -15,7 +15,7 @@ module.exports = function(app) {
     res.render("signup")
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("members");
@@ -25,8 +25,11 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
+  app.get("/members", isAuthenticated, function (req, res) {
     res.render("members");
   });
+  app.get("/movies", function (req, res) {
 
+    res.render("movies");
+  })
 };
